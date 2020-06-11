42a651e7efe482378f16528e07b03d5204045b60
chenlong <951709483@qq.com>
2020-06-11 17:12:35 +0800

    mybatis逆向工程

---
+++ src/main/java/com/lion/pojo/ComplicationsReport.java
@@ -0,0 +1,365 @@
+package com.lion.pojo;
+
+import java.util.Date;
+
+public class ComplicationsReport {
+    private String id;
+
+    private Date reportdate;
+
+    private Date createdate;
+
+    private Date updatedate;
+
+    private String baseinfoid;
+
+    private String baseuserinfoid;
+
+    private String registeredhospitalid;
+
+    private String registeredhospital;
+
+    private String patientname;
+
+    private Integer patientage;
+
+    private Integer stage;
+
+    private String telephone;
+
+    private String preoperativediagnosis;
+
+    private String firstoperationhospital;
+
+    private String firstoperationhospitalid;
+
+    private Date firstoperationdate;
+
+    private String firstoperationmethod;
+
+    private String netflixnameandcompany;
+
+    private String netflixnameandcompanyid;
+
+    private Integer type;
+
+    private Integer afteroperatiomonths;
+
+    private String complicationsdescription;
+
+    private String handle;
+
+    private String perioperativeperiodcomplications;
+
+    private String otherperioperativeperiodcomplications;
+
+    private String zyqcomplications;
+
+    private String otherzyqcomplications;
+
+    private Integer meshtype;
+
+    private String code;
+
+    private Integer meshtime;
+
+    private Integer mesh;
+
+    private Integer meshmince;
+
+    private Integer pain;
+
+    private Integer painposition;
+
+    private String othernetflixnameandcompany;
+
+    private Integer dindo;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getReportdate() {
+        return reportdate;
+    }
+
+    public void setReportdate(Date reportdate) {
+        this.reportdate = reportdate;
+    }
+
+    public Date getCreatedate() {
+        return createdate;
+    }
+
+    public void setCreatedate(Date createdate) {
+        this.createdate = createdate;
+    }
+
+    public Date getUpdatedate() {
+        return updatedate;
+    }
+
+    public void setUpdatedate(Date updatedate) {
+        this.updatedate = updatedate;
+    }
+
+    public String getBaseinfoid() {
+        return baseinfoid;
+    }
+
+    public void setBaseinfoid(String baseinfoid) {
+        this.baseinfoid = baseinfoid;
+    }
+
+    public String getBaseuserinfoid() {
+        return baseuserinfoid;
+    }
+
+    public void setBaseuserinfoid(String baseuserinfoid) {
+        this.baseuserinfoid = baseuserinfoid;
+    }
+
+    public String getRegisteredhospitalid() {
+        return registeredhospitalid;
+    }
+
+    public void setRegisteredhospitalid(String registeredhospitalid) {
+        this.registeredhospitalid = registeredhospitalid;
+    }
+
+    public String getRegisteredhospital() {
+        return registeredhospital;
+    }
+
+    public void setRegisteredhospital(String registeredhospital) {
+        this.registeredhospital = registeredhospital;
+    }
+
+    public String getPatientname() {
+        return patientname;
+    }
+
+    public void setPatientname(String patientname) {
+        this.patientname = patientname;
+    }
+
+    public Integer getPatientage() {
+        return patientage;
+    }
+
+    public void setPatientage(Integer patientage) {
+        this.patientage = patientage;
+    }
+
+    public Integer getStage() {
+        return stage;
+    }
+
+    public void setStage(Integer stage) {
+        this.stage = stage;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getPreoperativediagnosis() {
+        return preoperativediagnosis;
+    }
+
+    public void setPreoperativediagnosis(String preoperativediagnosis) {
+        this.preoperativediagnosis = preoperativediagnosis;
+    }
+
+    public String getFirstoperationhospital() {
+        return firstoperationhospital;
+    }
+
+    public void setFirstoperationhospital(String firstoperationhospital) {
+        this.firstoperationhospital = firstoperationhospital;
+    }
+
+    public String getFirstoperationhospitalid() {
+        return firstoperationhospitalid;
+    }
+
+    public void setFirstoperationhospitalid(String firstoperationhospitalid) {
+        this.firstoperationhospitalid = firstoperationhospitalid;
+    }
+
+    public Date getFirstoperationdate() {
+        return firstoperationdate;
+    }
+
+    public void setFirstoperationdate(Date firstoperationdate) {
+        this.firstoperationdate = firstoperationdate;
+    }
+
+    public String getFirstoperationmethod() {
+        return firstoperationmethod;
+    }
+
+    public void setFirstoperationmethod(String firstoperationmethod) {
+        this.firstoperationmethod = firstoperationmethod;
+    }
+
+    public String getNetflixnameandcompany() {
+        return netflixnameandcompany;
+    }
+
+    public void setNetflixnameandcompany(String netflixnameandcompany) {
+        this.netflixnameandcompany = netflixnameandcompany;
+    }
+
+    public String getNetflixnameandcompanyid() {
+        return netflixnameandcompanyid;
+    }
+
+    public void setNetflixnameandcompanyid(String netflixnameandcompanyid) {
+        this.netflixnameandcompanyid = netflixnameandcompanyid;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public Integer getAfteroperatiomonths() {
+        return afteroperatiomonths;
+    }
+
+    public void setAfteroperatiomonths(Integer afteroperatiomonths) {
+        this.afteroperatiomonths = afteroperatiomonths;
+    }
+
+    public String getComplicationsdescription() {
+        return complicationsdescription;
+    }
+
+    public void setComplicationsdescription(String complicationsdescription) {
+        this.complicationsdescription = complicationsdescription;
+    }
+
+    public String getHandle() {
+        return handle;
+    }
+
+    public void setHandle(String handle) {
+        this.handle = handle;
+    }
+
+    public String getPerioperativeperiodcomplications() {
+        return perioperativeperiodcomplications;
+    }
+
+    public void setPerioperativeperiodcomplications(String perioperativeperiodcomplications) {
+        this.perioperativeperiodcomplications = perioperativeperiodcomplications;
+    }
+
+    public String getOtherperioperativeperiodcomplications() {
+        return otherperioperativeperiodcomplications;
+    }
+
+    public void setOtherperioperativeperiodcomplications(String otherperioperativeperiodcomplications) {
+        this.otherperioperativeperiodcomplications = otherperioperativeperiodcomplications;
+    }
+
+    public String getZyqcomplications() {
+        return zyqcomplications;
+    }
+
+    public void setZyqcomplications(String zyqcomplications) {
+        this.zyqcomplications = zyqcomplications;
+    }
+
+    public String getOtherzyqcomplications() {
+        return otherzyqcomplications;
+    }
+
+    public void setOtherzyqcomplications(String otherzyqcomplications) {
+        this.otherzyqcomplications = otherzyqcomplications;
+    }
+
+    public Integer getMeshtype() {
+        return meshtype;
+    }
+
+    public void setMeshtype(Integer meshtype) {
+        this.meshtype = meshtype;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Integer getMeshtime() {
+        return meshtime;
+    }
+
+    public void setMeshtime(Integer meshtime) {
+        this.meshtime = meshtime;
+    }
+
+    public Integer getMesh() {
+        return mesh;
+    }
+
+    public void setMesh(Integer mesh) {
+        this.mesh = mesh;
+    }
+
+    public Integer getMeshmince() {
+        return meshmince;
+    }
+
+    public void setMeshmince(Integer meshmince) {
+        this.meshmince = meshmince;
+    }
+
+    public Integer getPain() {
+        return pain;
+    }
+
+    public void setPain(Integer pain) {
+        this.pain = pain;
+    }
+
+    public Integer getPainposition() {
+        return painposition;
+    }
+
+    public void setPainposition(Integer painposition) {
+        this.painposition = painposition;
+    }
+
+    public String getOthernetflixnameandcompany() {
+        return othernetflixnameandcompany;
+    }
+
+    public void setOthernetflixnameandcompany(String othernetflixnameandcompany) {
+        this.othernetflixnameandcompany = othernetflixnameandcompany;
+    }
+
+    public Integer getDindo() {
+        return dindo;
+    }
+
+    public void setDindo(Integer dindo) {
+        this.dindo = dindo;
+    }
+}
---
+++ src/main/java/utils/FullyAutomatic.java
@@ -0,0 +1,31 @@
+package utils;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author chenlong
+ * @version 1.0
+ * @description
+ * @date
+ */
+public class FullyAutomatic {
+    public static void main(String[] args) throws Exception{
+        List<String> warnings=new ArrayList<String>();
+        boolean overwrite=true;
+        File configFile=new File("D:\\develop\\IdeaProjects\\springboot\\mybatis_reverse_engineering\\src\\main\\resources\\config.xml");
+        ConfigurationParser cp=new ConfigurationParser(warnings);
+        Configuration config=cp.parseConfiguration(configFile);
+        DefaultShellCallback callback=new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator=new MyBatisGenerator(config,callback,warnings);
+        myBatisGenerator.generate(null);
+        System.out.println("生成成功");
+
+    }
+}
